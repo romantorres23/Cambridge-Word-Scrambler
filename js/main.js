aa33b433c6b511489c73db9a66db91221913443b
Roman Torres <rtorres@envivent.com>
2020-03-05 10:02:58 -0800

    basic setup of page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+class WordScrambler {
+    constructor(el) {
+
+    }
+
+    scramble() {
+        for (let i = 1; i < word.length - 1; i++) {
+            // console.log();
+        }
+    }
+}
+
+let ws = new WordScrambler(".scrambler");
